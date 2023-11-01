2b82ac33222d899f674aba4ca83972fc1c1f3441
DmytroDemyrskyi <long28469@gmail.com>
2023-11-02 00:21:30 +0200

    fix black

--- teachers/views.py
+++ teachers/views.py
@@ -5,6 +5,7 @@ from teachers.models import Teacher
 
 # Create your views here.
 
+
 def index(request):
     return HttpResponse("Nothing")
 
@@ -12,6 +13,9 @@ def index(request):
 def teachers_list(request):
     teachers = Teacher.objects.all()
     teacher_info = "\n".join(
-        [f"{teacher.id}. {teacher.name} {teacher.surname}  {teacher.birth_date}  {teacher.subject}" for teacher in
-         teachers])
+        [
+            f"{teacher.id}. {teacher.name} {teacher.surname}  {teacher.birth_date}  {teacher.subject}"
+            for teacher in teachers
+        ]
+    )
     return HttpResponse(teacher_info, content_type="text/plain")
