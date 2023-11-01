6f88cf4e2b7b4554a77d6d6c8f7ac09e7b6bb3a3
DmytroDemyrskyi <long28469@gmail.com>
2023-11-02 00:20:20 +0200

    add view

--- teachers/views.py
+++ teachers/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from teachers.models import Teacher
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Nothing")
+
+
+def teachers_list(request):
+    teachers = Teacher.objects.all()
+    teacher_info = "\n".join(
+        [f"{teacher.id}. {teacher.name} {teacher.surname}  {teacher.birth_date}  {teacher.subject}" for teacher in
+         teachers])
+    return HttpResponse(teacher_info, content_type="text/plain")
